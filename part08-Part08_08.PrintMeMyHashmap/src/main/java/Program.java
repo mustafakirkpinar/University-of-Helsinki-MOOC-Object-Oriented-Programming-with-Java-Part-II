af329d000d74829ba44caf2f5d8492020f25f0e4
Mustafa Kirkpinar <mustafakirkpinar@gmail.com>
2020-05-01 17:34:23 +0200

    Part 8-8 completed.

--- part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
+++ part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
@@ -3,6 +3,29 @@ import java.util.HashMap;
 
 public class Program {
 
+    public static void printKeys(HashMap<String, String> hashmap) {
+        for (String st : hashmap.keySet()) {
+            System.out.println(st);
+        }
+    }
+
+    public static void printKeysWhere(HashMap<String, String> hashmap, String text) {
+        for (String st : hashmap.keySet()) {
+            if (st.contains(text)) {
+                System.out.println(st);
+            }
+        }
+    }
+
+    public static void printValuesOfKeysWhere(HashMap<String, String> hashmap, String text) {
+        for (String st : hashmap.keySet()) {
+            if (st.contains(text)) {
+                System.out.println(hashmap.get(st));
+            }
+        }
+        
+    }
+
     public static void main(String[] args) {
         // Test your program here!
     }
