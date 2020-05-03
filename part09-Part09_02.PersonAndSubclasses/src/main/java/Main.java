edf4ada78f5bdce62aeeed3f1d397c0349fb0945
Mustafa Kirkpinar <mustafakirkpinar@gmail.com>
2020-05-03 04:07:58 +0200

    Part 9-2 completed.

--- part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
+++ part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
@@ -1,8 +1,47 @@
 
+import java.util.ArrayList;
+
 public class Main {
+
+    public static void printPersons(ArrayList<Person> persons) {
+        for (Person p : persons) {
+            System.out.println(p);;
+        }
+
+    }
 
     public static void main(String[] args) {
         // write your test code here
+        Person ada = new Person("Ada Lovelace", "24 Maddox St. London W1S 2QN");
+        Person esko = new Person("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki");
+        System.out.println(ada);
+        System.out.println(esko);
+
+        Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
+        System.out.println(ollie);
+        System.out.println("Study credits " + ollie.credits());
+        ollie.study();
+        System.out.println("Study credits " + ollie.credits());
+
+        ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
+        esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
+        System.out.println(ada);
+        System.out.println(esko);
+
+        ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
+
+        int i = 0;
+        while (i < 25) {
+            ollie.study();
+            i = i + 1;
+        }
+        System.out.println(ollie);
+
+        ArrayList<Person> persons = new ArrayList<Person>();
+        persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
+        persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
+
+        printPersons(persons);
     }
 
 }
