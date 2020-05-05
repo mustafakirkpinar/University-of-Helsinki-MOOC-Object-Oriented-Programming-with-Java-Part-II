c0f3603534c969c1262dc5b3add936442bcd156e
Mustafa Kirkpinar <mustafakirkpinar@gmail.com>
2020-05-06 00:38:22 +0200

    Part 10-5 completed.

--- part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -3,9 +3,20 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PrintingUserInput {
-
+    
     public static void main(String[] args) {
+        ArrayList<String> inputs = new ArrayList();
         Scanner scanner = new Scanner(System.in);
-
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.isEmpty()) {
+                break;
+            }
+            
+            inputs.add(input);
+            
+        }
+        inputs.forEach(s -> System.out.println(s));
     }
 }
