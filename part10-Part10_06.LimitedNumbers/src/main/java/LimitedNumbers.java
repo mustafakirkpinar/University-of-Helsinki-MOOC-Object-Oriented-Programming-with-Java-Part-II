6b71e31edea9ffb9aa87e2e47baca5d54bb58a83
Mustafa Kirkpinar <mustafakirkpinar@gmail.com>
2020-05-06 00:45:12 +0200

    Part 10-6 completed.

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -1,11 +1,21 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        List <Integer> inputs = new ArrayList();
+        int input;
+        while (true) {
+            input = Integer.valueOf(scanner.nextLine());
+            if (input < 0) {
+                break;
+            }
+            inputs.add(input);
+        }
+        inputs.stream().filter(i-> i<6 & i>0).forEach(i-> System.out.println(i));
     }
 }
