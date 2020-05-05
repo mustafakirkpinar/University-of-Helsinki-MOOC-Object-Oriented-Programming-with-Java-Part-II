c587a1df4aac2c05489617abc7a4070d85b836e0
Mustafa Kirkpinar <mustafakirkpinar@gmail.com>
2020-05-05 04:58:05 +0200

    Part 10-3 completed.

--- part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -1,9 +1,18 @@
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class PositiveNumbers {
+
+    public static List<Integer> positive(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(i-> i>=0)
+                .collect(Collectors.toCollection(ArrayList::new));
+        
+
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
