feca027594ce948e1a517aa72351812a9e46fe2e
Mustafa Kirkpinar <mustafakirkpinar@gmail.com>
2020-05-06 02:00:21 +0200

    Part 10-10 completed.

--- part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
+++ part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
@@ -1,4 +1,9 @@
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -7,7 +12,20 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class BooksFromFile {
-    
+    public static List<Book> readBooks(String file) throws IOException {
+         return Files.lines(Paths.get(file)).
+                map((String row) -> {
+                    String []a =row.split(",");
+                    String name= a[0];
+                    int year = Integer.valueOf(a[1]);
+                    int page = Integer.valueOf(a[2]);
+                    String author = a[3];
+                    Book book = new Book (name,year,page,author);
+                    return book;
+                    }).collect(Collectors.toList());
+        
+        
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test your method here
